2df2d0685cba403584a927cff9cf48ccdf92c2b0
Sunghwan-DS <jeonsung02@gmail.com>
2024-04-20 19:54:23 +0900

    Create 201 Response API

--- src/main/java/jsh/http/controller/statuscodetest/StatusCodePageApiController.java
+++ src/main/java/jsh/http/controller/statuscodetest/StatusCodePageApiController.java
@@ -1,9 +1,12 @@
 package jsh.http.controller.statuscodetest;
 
 import jsh.http.domain.MyResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/statuscodes")
@@ -11,6 +14,13 @@ public class StatusCodePageApiController {
 
     @GetMapping("/200")
     public MyResponse getRequest_200() {
-        return MyResponse.of("성공");
+        return MyResponse.of("200");
+    }
+
+    @GetMapping("/201")
+    public ResponseEntity<?> getRequest_201() {
+        URI location = URI.create("location");
+        return ResponseEntity.created(location)
+                             .body(MyResponse.of("201"));
     }
 }
